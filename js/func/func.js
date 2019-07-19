b084bc252602da2cb34e62b8b82184f6aa05a950
rontea <ronryanteano@gmail.com>
2019-07-19 14:22:27 -0400

    'added'

--- js/func/func.js
+++ js/func/func.js
@@ -4,25 +4,36 @@
 
 $(document).ready(function () {
 
-	$('.sidenav-click-open').click(function() {
-
+	pushContent();
 	
-			if($(this).hasClass('active')){
-				$('.sidenav').css("width","0");
-				$(this).removeClass('active');
-			
-				
-			}else{
-				$('.sidenav').css("width","250px");
-				$(this).addClass('active');
-			}	
+	
+	function pushContent(){
+		
+		
+		$('.sidenav-click-open').click(function() {
 
-	});
 
-	$('.sidenav-click-close').click(function() {
+				if($(this).hasClass('active')){
+					$('.sidenav').css("width","0");
+					
+					$('#main').css("margin-left","0");
+					$(this).removeClass('active');
 
-		$('.sidenav').css("width","0");
 
-	});
+				}else{
+					$('.sidenav').css("width","250px");
+					
+					$('#main').css("margin-left","250px");
+					$(this).addClass('active');
+				}	
+		});
+
+		$('.sidenav-click-close').click(function() {
+
+			$('.sidenav').css("width","0");
+
+		});	
+	}
+	
 
 });
